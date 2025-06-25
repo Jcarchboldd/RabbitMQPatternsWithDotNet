e414277139c37949431cfda67dbb46cec94a0771
Jair <jcarchboldd@outlook.com>
2025-06-25 20:22:25 +0200

    Add SendMessageEndPoints.cs Endpoint

---
+++ Producer.API/Producer/SendMessage/SendMessageEndPoints.cs
@@ -0,0 +1,6 @@
+namespace Producer.API.Producer.SendMessage;
+
+public class SendMessageEndPoints
+{
+    
+}
---
+++ Producer.API/Producer/SendMessage/SendMessageHandler.cs
@@ -0,0 +1,11 @@
+using Carter;
+
+namespace Producer.API.Producer.SendMessage;
+
+public class SendMessageHandler : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        throw new NotImplementedException();
+    }
+}
